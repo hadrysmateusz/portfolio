20ffe9078e83b4b4c5992b4fe3d0d31662924bfc
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-01-19 16:22:34 +0100

    create rudimentary 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import App from "../components/App"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+	<App>
+		<SEO title="Not Found" />
+		404 - Page Not Found
+	</App>
+)
+
+export default IndexPage
