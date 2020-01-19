74937adb16e6343495fb29445239b5678c93e9cc
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2020-01-19 18:26:09 +0100

    create constants file and add links

---
+++ src/const.js
@@ -0,0 +1,5 @@
+export const LINK_DRIBBBLE = "https://dribbble.com/MateuszHadrys"
+export const LINK_GITHUB = "https://github.com/hadrysmateusz"
+export const LINK_MEDIUM = "https://medium.com/@hadrysmateusz"
+export const LINK_TWITTER = "https://twitter.com/HadrysMateusz"
+export const EMAIL = "contact@hadrysmateusz.com"
