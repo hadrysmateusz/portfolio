1ba459be6d70df13e7be399045412f6559929aaa
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2021-03-11 17:24:01 +0100

    redo the page header

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,113 +1,70 @@
-import React, { useState } from "react"
+import React from "react"
 import styled from "styled-components"
 import { Link } from "gatsby"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
-import { useScrollPosition } from "@n8tb1t/use-scroll-position"
 
 import FluidContainer from "./FluidContainer"
 import SocialMedia from "./SocialMedia"
-import { LINK_MEDIUM, EMAIL } from "../const"
 
 const PageHeader = () => {
-	const [showSocial, setShowSocial] = useState(false)
-
-	useScrollPosition(
-		({ currPos }) => {
-			const shouldShow = currPos.y > 220
-			if (shouldShow !== showSocial) setShowSocial(shouldShow)
-		},
-		[showSocial],
-		null,
-		true
-	)
-
-	const data = useStaticQuery(graphql`
-		query {
-			allFile(filter: { name: { eq: "rounded-corner" } }) {
-				nodes {
-					childImageSharp {
-						fluid(fit: COVER) {
-							...GatsbyImageSharpFluid
-						}
-					}
-				}
-			}
-		}
-	`)
-
-	const roundedCorner = data.allFile.nodes[0].childImageSharp.fluid
-
-	return (
-		<Wrapper>
-			<FluidContainer>
-				<InnerContainer>
-					<Section>{showSocial && <SocialMedia />}</Section>
-					<Section>
-						<NavLink to="#projects">Projects</NavLink>
-						{/* TODO: replace with internal link to personal blog */}
-						<NavLink as="a" href={LINK_MEDIUM}>
-							Blog
-						</NavLink>
-						<NavLink as="a" href={"mailto:" + EMAIL}>
-							Contact
-						</NavLink>
-					</Section>
-				</InnerContainer>
-			</FluidContainer>
-			<ImageContainer>
-				<Img fluid={roundedCorner} />
-			</ImageContainer>
-		</Wrapper>
-	)
+  return (
+    <Wrapper>
+      <FluidContainer>
+        <InnerContainer>
+          <Section>
+            <NavLink to="/#about">About</NavLink>
+            <NavLink to="/#projects">Projects</NavLink>
+            {/* TODO: replace with internal link to personal blog */}
+            <NavLink to="/#blog">Blog</NavLink>
+            <NavLink to="/#contact">Contact</NavLink>
+          </Section>
+          <Section>
+            <SocialMedia />
+          </Section>
+        </InnerContainer>
+      </FluidContainer>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
-	position: fixed;
-	z-index: 1000;
-	top: 0;
-	left: 0;
-	width: 100%;
-	background: black;
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background: var(--gray-100);
 `
 
-const ImageContainer = styled.div`
-	position: absolute;
-	top: 100%;
-	width: var(--border-radius-big);
-	height: var(--border-radius-big);
-`
-
-const InnerContainer = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	height: var(--header-height);
+const InnerContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: var(--header-height);
 `
 
 const Section = styled.div`
-	display: grid;
-	grid-auto-flow: column;
-	gap: 12px;
-	@media (min-width: 400px) {
-		gap: 16px;
-	}
-	@media (min-width: 732px) {
-		gap: 32px;
-	}
+  display: grid;
+  grid-auto-flow: column;
+  gap: 12px;
+  @media (min-width: 400px) {
+    gap: 16px;
+  }
+  @media (min-width: 732px) {
+    gap: 32px;
+  }
 `
 
 const NavLink = styled(Link)`
-	text-transform: uppercase;
-	font-weight: var(--fw-bold);
-	letter-spacing: -0.01em;
-	font-size: 11px;
-	@media (min-width: 400px) {
-		font-size: 12px;
-	}
-	@media (min-width: 732px) {
-		font-size: 24px;
-	}
+  text-transform: uppercase;
+  font-weight: var(--fw-bold);
+  letter-spacing: -0.01em;
+  font-size: 11px;
+  @media (min-width: 400px) {
+    font-size: 12px;
+  }
+  @media (min-width: 732px) {
+    font-size: 24px;
+  }
 `
 
 export default PageHeader
